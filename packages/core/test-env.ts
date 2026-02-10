4a4f90f5dbb72828600f502a376460cad485e2e9
dongzhaokun <dongzhaokun@bigai.ai>
2026-02-10 15:01:03 +0800

    delete some useless files

--- packages/core/test-env.ts
+++
@@ -1,5 +0,0 @@
-console.log('Environment variables:');
-console.log('LOG_FILE:', process.env.LOG_FILE);
-console.log('LOG_LEVEL:', process.env.LOG_LEVEL);
-console.log('LLM_MODEL:', process.env.LLM_MODEL);
-console.log('PORT:', process.env.PORT);
