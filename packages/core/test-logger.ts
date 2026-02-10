0cc90f5551b520ba4334cd99416089b78770aa9c
dongzhaokun <gddzhaokun@gmail.com>
2026-02-10 15:01:03 +0800

    delete some useless files

--- packages/core/test-logger.ts
+++
@@ -1,9 +0,0 @@
-import { createLogger } from "./src/utils/logger.js";
-
-const logger = createLogger("test");
-
-logger.info("Test info message");
-logger.debug("Test debug message", { key: "value" });
-logger.error("Test error message");
-
-console.log("Logger test completed");
